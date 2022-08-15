ee89f1109790e9071100d9f8dd3eaf114e8d417d
olga_lihaia <olga.l@usenergysolutions.us>
2022-08-15 17:28:04 +0300

    start movie-app

---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+const colors = require('tailwindcss/colors')
+const plugins = require('tailwindcss/plugin')
+
+const primary = '#E30B13'
+
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./app/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            primary,
+            black: colors.black,
+            white: colors.white,
+            transparent: colors.transparent,
+            yellow: {
+                700: '#F5C521'
+            },
+            gray: {
+                300: '#d9dae8',
+                500: '#999AA5',
+                600: '#66676E',
+                700: '#39393f',
+                800: '#242529',
+                900: '#191B1F',
+                950: '#101215',
+            }
+
+        },
+        extend: {
+            spacing: {
+                0.5: '0.12rem',
+                layout: '2.75rem',
+            },
+            fontsize: {
+                '2lg': '1.38rem',
+            },
+            borderRadius: {
+                image: '0.5rem',
+                layout: '0.8rem'
+            },
+            transitionTimingFunction:{
+                DEFAULT: 'easy-in-out'
+            },
+            transitionDuration:{
+                DEFAULT: '200ms'
+            },
+            zIndex: {
+                1: '1',
+                2: '2',
+                3: '3'
+            },
+            keyframes: {
+                fade:{
+                    from: {opacity: 0},
+                    to: {opacity: 1}
+                },
+                scaleIn: {
+                    '0%':{
+                        opacity: 0,
+                        transform: 'scale(0.9)',
+                    },
+                    '50%':{
+                        opacity: 0.3,
+                    },
+                    '100%':{
+                        opacity: 1,
+                        transform: 'scale(1)',
+                    },
+                }
+            },
+            animation:{
+                fade: 'fade .5s easy-in-out',
+            }
+        },
+    },
+    plugins: [],
+}
